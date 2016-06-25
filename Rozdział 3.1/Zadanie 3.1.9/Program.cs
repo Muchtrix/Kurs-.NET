746620a59e5432fa416ad0d0949616c35de5bc3d
Muchtrix <muchtrix1995@gmail.com>
2016-06-25 11:41:16 +0200

    Final

---
+++ Rozdział 3.1/Zadanie 3.1.9/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_3._1._9 {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
---
+++ Rozdział 3.3/Zadanie 3.3.2/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zadanie_3._3._2 {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void ProgressBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
+
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e) {
+            bar.Value += 10;
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e) {
+            bar.Value -= 10;
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e) {
+            bar.Value = bar.Minimum;
+        }
+
+        private void mediaElement_MediaEnded(object sender, RoutedEventArgs e) {
+            mediaElement.Position = new TimeSpan(0, 0, 1);
+            mediaElement.Play();
+        }
+    }
+}
